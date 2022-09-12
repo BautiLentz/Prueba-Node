42a1f698628694f63b20f00188e84c3127e73770
BautiLentz <bautistalentz@gmail.com>
2022-09-12 17:19:51 -0300

    Nueva funcion de Perfiles

--- xd.js
+++ xd.js
@@ -1,1 +1,15 @@
-console.log("Hola")
+
+function Perfiles(nombre,apellido,vocacion,nacionalidad,domicilio,edad,dni){
+    this.nombre= nombre;
+    this.apellido= apellido;
+    this.vocacion=vocacion;
+    this.nacionalidad=nacionalidad;
+    this.domicilio=domicilio;
+    this.edad=edad;
+    this.dni=dni;
+}
+
+let persona1=new Perfiles("Juan","Carubin","Estudiante","Argentino","CABA",19,45076890)
+
+let persona2=new Perfiles("Bautista",'Lentz',"Programacion",'Argentino',"CABA",20,44678907)
+console.log(persona1.nacionalidad)
